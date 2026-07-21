1386d330648848125c169e359b0f46eb8cd9eba9
Michael Rivera <146748687+MikeMan161@users.noreply.github.com>
2026-07-20 21:33:14 -0400

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.py
@@ -0,0 +1,39 @@
+class TimeMap:
+
+    def __init__(self):
+        #initialize the dictionary
+        self.hashmap = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        #insert the values into the hashmap
+        #the key-value goes like this: [key: [timestamp, value]]
+        if key not in self.hashmap:
+            self.hashmap[key] = []
+        self.hashmap[key].append([timestamp, value])
+
+
+    def get(self, key: str, timestamp: int) -> str:
+        #This is where we search the hashmap to find either the given timestamp
+        #or the largest timestamp less than timestamp, aka left boundary binary search
+        if key not in self.hashmap:
+            return ""
+        timeList = list(self.hashmap[key])
+        left = 0
+        right = len(timeList) - 1
+        result = ""
+        while left <= right:
+            mid = left + (right - left) // 2
+            if timeList[mid][0] == timestamp:
+                result = timeList[mid][1]
+                return result
+            if timeList[mid][0] <= timestamp:
+                result = timeList[mid][1]
+                left = mid + 1
+            else:
+                right = mid - 1
+        return result
+
+
+
+
+        
